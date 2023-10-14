602136bd57a6c268dd58629e037582294331ebc2
shwarzbergzelda <zeldashw@gmail.com>
2023-10-14 02:50:26 -0400

    fix components import

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { View, Text, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'react-router';
 
 import { COLORS, icons, images, SIZES } from  '../constants';
-import { NearbyJobs, PopularJobs, ScreenHeaderBtn, Welcome } from '../components';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
     return (
