57dd339eeb8c08c56b543bef382e0232674c21c1
shwarzbergzelda <zeldashw@gmail.com>
2023-10-14 02:49:44 -0400

    import relevant components, hooks

--- app/index.js
+++ app/index.js
@@ -1,4 +1,9 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'react-router';
+
+import { COLORS, icons, images, SIZES } from  '../constants';
+import { NearbyJobs, PopularJobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
     return (
